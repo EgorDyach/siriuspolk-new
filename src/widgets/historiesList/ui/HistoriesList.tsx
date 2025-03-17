cf6e93196960475f475ab8fd4b57e66114d3d4fe
EgorDyach <tvoysosed33@gmail.com>
2025-03-17 21:37:38 +0300

    feat: added search

--- src/widgets/historiesList/ui/HistoriesList.tsx
+++ src/widgets/historiesList/ui/HistoriesList.tsx
@@ -1,18 +1,44 @@
+'use client';
 import HistoriesCard from '@features/histories-card/ui/HistoriesCard';
+import { useDebounce } from '@shared/lib/hooks/useDebounce';
 import { ShortPerson } from '@shared/model/types';
-import React, { FC } from 'react';
+import { Input } from '@shared/ui/Input';
+import React, { FC, useState } from 'react';
 
 interface HistoriesListProps {
   histories: ShortPerson[];
 }
 
 const HistoriesList: FC<HistoriesListProps> = ({ histories }) => {
+  const [value, setValue] = useState('');
+
+  const filter = useDebounce(value, 300);
+
   return (
-    <div className="grid grid-cols-3 gap-[35px] auto-rows-fr">
-      {histories.map((item, index) => (
-        <HistoriesCard key={index} item={item} />
-      ))}
-    </div>
+    <>
+      <Input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Найти..."
+        className="mb-[24px]"
+      />
+      <div className="grid grid-cols-3 gap-[35px] auto-rows-fr">
+        {histories
+          .filter((item) =>
+            item.SNL.toLowerCase().includes(filter.trim().toLowerCase()),
+          )
+          .map((item, index) => (
+            <HistoriesCard key={index} item={item} />
+          ))}
+      </div>
+      {!histories.filter((item) =>
+        item.SNL.toLowerCase().includes(filter.trim().toLowerCase()),
+      ).length && (
+        <p className="text-gray-500 italic text-center w-full">
+          Не удалось найти истории по вашему запросу!
+        </p>
+      )}
+    </>
   );
 };
 
