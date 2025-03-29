4acb098cf1e9be8a7a7685259be70d50114ea5e4
EgorDyach <tvoysosed33@gmail.com>
2025-03-29 16:08:12 +0300

    style: adaptive for not found

--- src/pages/notFound/ui/NotFound.tsx
+++ src/pages/notFound/ui/NotFound.tsx
@@ -2,17 +2,17 @@ import Link from 'next/link';
 
 export default async function NotFound() {
   return (
-    <section className="pt-[120px] h-[65vh]">
+    <section className="pt-10 xl:pt-[100px] h-[65vh]">
       <div className="container">
-        <h1 className="font-bold font-lora text-7xl mb-8">
+        <h1 className="text-4xl  font-bold font-lora xl:text-7xl mb-4 xl:mb-8">
           Страница не найдена
         </h1>
-        <p className="text-2xl max-w-[600px] mb-32">
+        <p className="text-[14px] xl:text-2xl max-w-[600px] mb-8 xl:mb-24">
           Страница, которую вы ищете, не существует. Возможно, она была
           перемещена или удалена. Попробуйте перейти на главную.
         </p>
         <Link
-          className=" text-white outline-offset-[-3px] font-lora text-2xl/[1] relative transition-all focus:outline-white focus:outline-3 hover:text-[#989898] data-[active=true]:bg-[#494E53] bg-[#343434] py-[36px] px-[65px]"
+          className="text-[14px] py-3.5 px-8 text-white outline-offset-[-3px] font-lora xl:text-2xl/[1] relative transition-all focus:outline-white focus:outline-3 hover:text-[#989898] data-[active=true]:bg-[#494E53] bg-[#343434] xl:py-[36px] xl:px-[65px]"
           href={'/'}
         >
           Вернуться на главную
