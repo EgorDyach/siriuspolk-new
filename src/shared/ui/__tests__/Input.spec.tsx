d36fc916a13de29042141f8ab4219c1eb1c18ca2
EgorDyach <tvoysosed33@gmail.com>
2025-04-09 18:04:00 +0300

    test: added onChange test for input

--- src/shared/ui/__tests__/Input.spec.tsx
+++ src/shared/ui/__tests__/Input.spec.tsx
@@ -1,4 +1,4 @@
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import { Input } from '../Input';
 
 describe('<Input />', () => {
@@ -6,15 +6,29 @@ describe('<Input />', () => {
     render(<Input />);
     expect(screen.getByTestId('input')).toBeInTheDocument();
   });
+
+  it('should correctly work onChange', () => {
+    const onChange = jest.fn();
+    render(<Input onChange={onChange} />);
+    fireEvent.change(screen.getByTestId('input'), {
+      target: {
+        value: 'test-value',
+      },
+    });
+    expect(onChange).toHaveBeenCalled();
+  });
+
   it('should render correctly with classname', () => {
     render(<Input className="test-class" />);
     expect(screen.getByTestId('label')).toHaveClass('test-class');
   });
+
   it('should render correctly with required', () => {
     render(<Input required />);
     expect(screen.getByTestId('input')).toBeRequired();
     expect(screen.getByTestId('required-mark')).toBeInTheDocument();
   });
+
   it('should render correctly with different types', () => {
     render(<Input type="email" />);
     expect(screen.getByTestId('input')).toHaveAttribute('type', 'email');
@@ -25,27 +39,32 @@ describe('<Input />', () => {
     render(<Input type="file" />);
     expect(screen.getByTestId('input')).toHaveAttribute('type', 'file');
   });
+
   it('should render correctly with label', () => {
     render(<Input label="Test input" />);
     expect(screen.getByTestId('input')).toBeInTheDocument();
     expect(screen.getByText(/test input/i)).toBeInTheDocument();
   });
+
   it('should render correctly with error', () => {
     render(<Input error="Test error" />);
     expect(screen.getByTestId('input')).toBeInTheDocument();
     expect(screen.getByText(/test error/i)).toBeInTheDocument();
   });
+
   it('should connect name and htmlFor', () => {
     render(<Input name="test-name" />);
     expect(screen.getByTestId('input')).toHaveAttribute('name', 'test-name');
     expect(screen.getByTestId('label')).toHaveAttribute('for', 'test-name');
   });
+
   it('should render correctly with other props', () => {
     render(<Input disabled style={{ color: 'red' }} placeholder="test" />);
     expect(screen.getByTestId('input')).toBeDisabled();
     expect(screen.getByTestId('input')).toHaveAttribute('placeholder', 'test');
     expect(screen.getByTestId('input')).toHaveStyle('color: red');
   });
+
   it('matches snapshot with default input', () => {
     const { asFragment } = render(<Input />);
     expect(asFragment()).toMatchSnapshot();
