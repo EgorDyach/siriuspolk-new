44f80bc8a50b4f8b0839b8530d7946c8de07b091
EgorDyach <tvoysosed33@gmail.com>
2025-04-20 17:31:35 +0300

    fix: new types on shared

--- src/shared/model/types.ts
+++ src/shared/model/types.ts
@@ -1,16 +1,27 @@
 export type Person = {
-  id: number;
-  city: string;
+  id: string;
+  name: string;
+  surname: string;
+  patronymic: string;
   date_birth: number;
   date_death: number;
+  city: string;
   history: string;
-  role: boolean;
-  main_photo: string;
-  medals: string;
-  SNL: string;
-  photo: string;
   rank: string;
-  date_pulished: number;
+  contact_email: string;
+  contact_name: string;
+  contact_surname: string;
+  contact_patronymic: string;
+  contact_telegram: string;
+  medals: Medal[];
+  relative: string;
+  url: string;
+};
+
+export type Medal = {
+  photo_link: string;
+  name: string;
+  id: number;
 };
 
 export type ShortPerson = {
