6d37d6d77197b3ffb1528a7e2d5f40603ec0ac03
EgorDyach <tvoysosed33@gmail.com>
2025-04-20 15:08:37 +0300

    fix: added props for button

--- src/shared/ui/Button.tsx
+++ src/shared/ui/Button.tsx
@@ -3,16 +3,26 @@ import React, { ComponentProps, ReactNode } from 'react';
 
 interface ButtonProps extends ComponentProps<'button'> {
   children?: ReactNode;
+  size?: 'small' | 'default';
+  theme?: 'gray' | 'default';
 }
 
-function Button({ className, children, ...props }: ButtonProps) {
+function Button({
+  className,
+  children,
+  size = 'default',
+  theme = 'default',
+  ...props
+}: ButtonProps) {
   return (
     <>
       <button
         className={cx(
+          className,
+          theme === 'gray' && 'bg-[#D9D9D9] !text-black [&>*]:text-black',
+          size === 'small' && 'text-sm md:text-base',
           'py-[12px] px-[32px] text-white text-base md:text-2xl font-lora bg-[#343434] cursor-pointer',
           'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
-          className,
         )}
         {...props}
       >
