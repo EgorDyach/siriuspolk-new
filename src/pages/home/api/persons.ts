80009c4e40e94fd0a5f96cd8ee061ea7f44a2d70
EgorDyach <tvoysosed33@gmail.com>
2025-05-02 14:10:49 +0300

    fix: fixed request link

--- src/pages/home/api/persons.ts
+++ src/pages/home/api/persons.ts
@@ -2,5 +2,5 @@ import { serverRequest } from '@shared/api';
 import { Person } from '@shared/model/types';
 
 export const requestPersons = async (): Promise<Person[]> => {
-  return await serverRequest.get('/person?check=true');
+  return await serverRequest.get('/person?check=true&status=true');
 };
